a5fd7122abc49d48932978030b612230659dfb10
Enqirue <excastillo@oakland.edu>
2022-03-02 17:55:02 -0500

    Initial frontend server code

---
+++ spaceroombas/server.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+app = Flask(name)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route('/api/v1', methods=["GET"])
+def info_view():
+    """List of routes for this API."""
+    output = {
+            "Server": "placeholder.class",
+            "IP": "IP.class"
+
+    }
+    return output
+
+@app.route("/post", methods = ['POST'])
+def postJson():
+    print (request.is_json)
+    content = request.get_json()
+    print(content)
+    print(content['device'])
+    print(content['value'])
+    print(content['timestamp'])
+    return content['timestamp']
