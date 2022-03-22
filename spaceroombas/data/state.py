5d8a1da7a2422b3a0c36fb0257409ae946dd1039
AndersenBall <andersen.ball@gmail.com>
2022-03-22 12:45:33 -0400

    Completed Sector Logic. Sectors auto connect.

--- spaceroombas/data/state.py
+++ spaceroombas/data/state.py
@@ -17,6 +17,12 @@ class MapSector():
         self.sect_down = None
         self.sect_left = None
         self.sect_right = None
+    def parse_id(sector_id):
+        x,y = sector_id.split(",")
+        return int(x),int(y)
+
+    def form_sector_id(x,y):
+        return str(x)+","+str(y)
 
 class EntityLocation():
     """
@@ -34,12 +40,35 @@ class MapState():
         self.__sectors = dict()
         self.generate_map_sector("0,0")
     
-    def parse_id(sector_id):
-        x,y = sector_id.split(",")
-        return int(x),int(y)
+    def connect_sectors(self,sector1id,sector2id):
+        if self.__sectors.get(sector1id) != None and self.__sectors.get(sector2id) != None:
+            sector1 = self.__sectors.get(sector1id)
+            sector2 = self.__sectors.get(sector2id)
+            x1,y1= MapSector.parse_id(sector1id)
+            x2,y2= MapSector.parse_id(sector2id)
 
-    def form_sector_id(x,y):
-        return str(x)+","+str(y)
+            if(x1-1==x2 and y1==y2): #if sec2 is to the left
+                sector1.sect_left = sector2
+                sector2.sect_right = sector1
+                return True
+            elif(x1+1==x2 and y1==y2): #if sec2 is to the right
+                sector1.sect_right = sector2
+                sector2.sect_left = sector1
+                return True
+            elif(x1==x2 and y1+1==y2): #if sec2 is up
+                sector1.sect_up = sector2
+                sector2.sect_down = sector1
+                return True
+            elif(x1==x2 and y1-1==y2): #if sec2 is down
+                sector1.sect_down = sector2
+                sector2.sect_up = sector1
+                return True
+            else:
+                print("sectors do not connect",sector1id,sector2id)
+                return False
+        else:
+            print("sectors do not exist in map",sector1id,sector2id)
+            return False
 
     def get_sector(self, sector_id):
         if self.__sectors.get(sector_id) == None:
@@ -55,28 +84,41 @@ class MapState():
     def generate_map_sector(self,sector_id): ## TODO incorporate logic for adding to current graph/ update ID
         if self.__sectors.get(sector_id) == None:
             self.__sectors[sector_id] = MapSector(sector_id)
-        else:
-            print("error sector already created")##TODO idk if this should be a true error or just something that happens
+            x,y= MapSector.parse_id(sector_id)
+            self.connect_sectors(sector_id, MapSector.form_sector_id(x+1,y))
+            self.connect_sectors(sector_id, MapSector.form_sector_id(x-1,y))
+            self.connect_sectors(sector_id, MapSector.form_sector_id(x,y+1))
+            self.connect_sectors(sector_id, MapSector.form_sector_id(x,y-1))
+            return True
 
+        else:
+            print("error sector already created")
+            return False
     def update_walkable(self,location: EntityLocation, walkable):
         current_sector = self.get_sector(location.sector.sector_id)
         current_sector.land_map[location.x][location.y][0] = walkable
 
-    def check_tile_used(self,location: EntityLocation):
-        
+    def check_tile_used(self,location: EntityLocation):   
         current_sector = self.get_sector(location.sector.sector_id)
         return current_sector.land_map[location.x][location.y][0]
 
 
-    
-class PlayerRobot(): 
+class GameObject(): #TODO game object... we can have buildings ext
     def __init__(self, owner_id, location: EntityLocation):
         self.owner = owner_id
         self.location = location
         self.firmware = None #TODO add current firmware/ function being run by robot
 
-class PlayerState():
+class PlayerRobot():#TODO make player robot inherit from a general game object 
+    robot_count = 0
+    def __init__(self, owner_id, location: EntityLocation):
+        self.owner = owner_id
+        self.robot_id = "r"+str(self.robot_count)
+        self.robot_count += 1
+        self.location = location
+        self.firmware = None #TODO add current firmware/ function being run by robot
 
+class PlayerState():
     def __init__(self, player_id,sector_id):
         self.home_sector = sector_id
         self.player_id = player_id
@@ -98,19 +140,23 @@ class GameState():
 
         if player_id in self.players:## TODO implement logic here for orphan
             raise PlayerExistsError("Player already exists")
+
         self.players[player_id] = playerState
         x=20#TODO implement logic to spawn starting robot not in a used tile
         y=20
         self.add_robot(player_id,EntityLocation(sector,x,y))
 
     def add_robot(self,player_id,location: EntityLocation):
-        print(self.map.check_tile_used(location))
         if self.players.get(player_id) == None:
             print("invalid player id to add robot too")
+            return False
         else:
             self.players[player_id].add_robot(location)
             self.map.update_walkable(location,False)
-        print(self.map.check_tile_used(location))
-# TODO write code to do robot movement.... include moving in map and sector.
+            return True
+
+
+    def move_robot_left(self,player_id,robot_id):
+        return
 
 # TODO logic for orphan client
--- spaceroombas/test/test_game_state.py
+++ spaceroombas/test/test_game_state.py
@@ -10,34 +10,122 @@ def test_expand_map(): #checks to see if map has walkable value and value for th
 
 def test_sector_parsor():
     sector_id = "0,0"
-    assert state.MapState.parse_id(sector_id) == (0,0)
+    assert state.MapSector.parse_id(sector_id) == (0,0)
     sector_id = "203,192"
-    assert state.MapState.parse_id(sector_id) == (203,192)
+    assert state.MapSector.parse_id(sector_id) == (203,192)
     sector_id = "-203,192"
-    assert state.MapState.parse_id(sector_id) == (-203,192)
+    assert state.MapSector.parse_id(sector_id) == (-203,192)
     sector_id = "-203,-192"
-    assert state.MapState.parse_id(sector_id) == (-203,-192)
+    assert state.MapSector.parse_id(sector_id) == (-203,-192)
     sector_id = "203,-192"
-    assert state.MapState.parse_id(sector_id) == (203,-192)
+    assert state.MapSector.parse_id(sector_id) == (203,-192)
 
 def test_sectorid_fromindex():
     x=0
     y=0
-    assert state.MapState.form_sector_id(x,y) =="0,0"
+    assert state.MapSector.form_sector_id(x,y) =="0,0"
     x=-1
     y=-2131
-    assert state.MapState.form_sector_id(x,y) =="-1,-2131"
+    assert state.MapSector.form_sector_id(x,y) =="-1,-2131"
     x=12
     y=-2131
-    assert state.MapState.form_sector_id(x,y) =="12,-2131"
+    assert state.MapSector.form_sector_id(x,y) =="12,-2131"
 
-def test_new_sector():
+def test_duplicate_sector():
     game_state = state.GameState()
     assert game_state.map.sector_count() == 1
-    game_state.map.generate_map_sector("0,0")
+    assert game_state.map.generate_map_sector("0,0") == False
     assert game_state.map.sector_count() == 1
-    game_state.map.generate_map_sector("1,0")
+    assert game_state.map.generate_map_sector("1,0") == True
     assert game_state.map.sector_count() == 2
+
+def test_connecting_sectors():
+    game_state = state.GameState()
+    sector0 = game_state.map.get_sector("0,0")
+
+    game_state.map.generate_map_sector("0,1")
+    sector1 = game_state.map.get_sector("0,1")
+    assert game_state.map.connect_sectors(sector0.sector_id,sector1.sector_id) == True
+    assert sector0.sect_up == sector1
+    assert sector1.sect_down == sector0
+
+    game_state.map.generate_map_sector("1,0")
+    sector2 = game_state.map.get_sector("1,0")
+    assert game_state.map.connect_sectors(sector0.sector_id,sector2.sector_id) == True
+    assert sector0.sect_right == sector2
+    assert sector2.sect_left == sector0
+
+    game_state.map.generate_map_sector("-1,0")
+    sector3 = game_state.map.get_sector("-1,0")
+    assert game_state.map.connect_sectors(sector0.sector_id,sector3.sector_id) == True
+    assert sector0.sect_left == sector3
+    assert sector3.sect_right == sector0
+
+    game_state.map.generate_map_sector("0,-1")
+    sector4 = game_state.map.get_sector("0,-1")
+    assert game_state.map.connect_sectors(sector0.sector_id,sector4.sector_id) == True
+    assert sector0.sect_down == sector4
+    assert sector4.sect_up == sector0
+
+    game_state.map.generate_map_sector("0,-20")
+    sector5 = game_state.map.get_sector("0,-20")
+    print(sector5.sector_id)
+    print(sector0.sector_id)
+    assert game_state.map.connect_sectors(sector0.sector_id,sector5.sector_id) == False
+    
+
+def test_adding_new_sectors():
+    game_state = state.GameState()
+    game_state.map.generate_map_sector("1,0")
+    game_state.map.generate_map_sector("-1,0")
+    game_state.map.generate_map_sector("0,1")
+    game_state.map.generate_map_sector("0,-1")
+    game_state.map.generate_map_sector("1,1")
+    
+    sector0 = game_state.map.get_sector("0,0")
+    sector1 = game_state.map.get_sector("1,0")
+    assert sector0.sect_right == sector1
+    assert sector1.sect_left == sector0
+
+    sector1 = game_state.map.get_sector("-1,0")
+    assert sector0.sect_left == sector1
+    assert sector1.sect_right == sector0
+
+    sector1 = game_state.map.get_sector("0,1")
+    assert sector0.sect_up == sector1
+    assert sector1.sect_down == sector0
+
+    sector1 = game_state.map.get_sector("0,-1")
+    assert sector0.sect_down == sector1
+    assert sector1.sect_up == sector0
+
+    sector0 = game_state.map.get_sector("1,0")
+    sector1 = game_state.map.get_sector("0,1")
+    sector2 = game_state.map.get_sector("1,1")
+    assert sector0.sect_up == sector2
+    assert sector2.sect_down == sector0
+    assert sector1.sect_right == sector2
+    assert sector2.sect_left == sector1
+
+def test_adding_new_sectors_hard():
+    game_state = state.GameState()
+    game_state.map.generate_map_sector("3,0")
+    game_state.map.generate_map_sector("1,0")
+    game_state.map.generate_map_sector("2,0")
+
+    sector0 = game_state.map.get_sector("0,0")
+    sector1 = game_state.map.get_sector("1,0")
+    sector2 = game_state.map.get_sector("2,0")
+    sector3 = game_state.map.get_sector("3,0")
+    
+    assert sector0.sect_right == sector1
+    assert sector1.sect_left == sector0
+    assert sector1.sect_right == sector2
+    assert sector2.sect_left == sector1
+    assert sector2.sect_right == sector3
+    assert sector3.sect_left == sector2
+
+
 
 def test_change_walkable():
     game_state = state.GameState()
@@ -54,13 +142,12 @@ def test_change_walkable():
 def test_spawn_player():
     game_state = state.GameState()
     game_state.add_player("player1")
-    
+    assert len(game_state.players)==1
     
 def test_walkable():
     game_state = state.GameState()
     game_state.add_player("player1")
     assert game_state.map.check_tile_used(game_state.players["player1"].robots[0].location) == False
-
 
 
 
