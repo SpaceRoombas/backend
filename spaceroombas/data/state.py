d557659c629efa54647f4f143f837fec6b768577
Cody Huzarski <chuzarski@gmail.com>
2022-04-04 23:29:17 -0400

    Message additions (#20)
    * Added message for incoming player code changes and associated logic. Added logic to apply player code changes. Adjusted robot logic to reinitialize Interpreter on firmware change
    
    * Refactored robot movement code down to one function.
    
    * Added support to send robot movement change updates down to clients
    
    * Fixed an issue

--- spaceroombas/data/state.py
+++ spaceroombas/data/state.py
@@ -258,6 +258,18 @@ class GameState:
 
     # Aliases for robot movement
     def move_robot_up(self, player_id, robot_id):
+<<<<<<< HEAD
+        return self.move_player_robot(player_id, robot_id, 0, 1)
+
+    def move_robot_down(self, player_id, robot_id):
+        return self.move_player_robot(player_id, robot_id, 0, -1)
+
+    def move_robot_left(self, player_id, robot_id):
+        return self.move_player_robot(player_id, robot_id, -1, 0)
+
+    def move_robot_right(self, player_id, robot_id):
+        return self.move_player_robot(player_id, robot_id, 1, 0)
+=======
         return self.move_player_robot(player_id, robot_id, 1, 0)
 
     def move_robot_down(self, player_id, robot_id):
@@ -268,6 +280,7 @@ class GameState:
 
     def move_robot_right(self, player_id, robot_id):
         return self.move_player_robot(player_id, robot_id, 0, 1)
+>>>>>>> 7f897d13918cc271ebd3be58f82c54c781ea9eee
 
 # TODO add logic for movement between sectors, check movement code to be neater
 
